9a6d897614a44899e5988e2f372faf94261c02b3
Jason Heylon <jasonheylon@gmail.com>
2020-04-08 01:02:40 +0800

    Updates

---
+++ precache-manifest.382fdc4cc9caad31fe5f7ea6f52198f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d968f0c593e54d8463317b4ad251f931",
+    "url": "/react-use-confirm-leave/index.html"
+  },
+  {
+    "revision": "fc5eb95472172a9c9c33",
+    "url": "/react-use-confirm-leave/static/css/main.b18535c0.chunk.css"
+  },
+  {
+    "revision": "6a1cc9bda9de93221bfd",
+    "url": "/react-use-confirm-leave/static/js/2.d0ba9ff3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-use-confirm-leave/static/js/2.d0ba9ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc5eb95472172a9c9c33",
+    "url": "/react-use-confirm-leave/static/js/main.ddf864d3.chunk.js"
+  },
+  {
+    "revision": "131911d4d343e44b3d58",
+    "url": "/react-use-confirm-leave/static/js/runtime-main.39b43354.js"
+  }
+]);
